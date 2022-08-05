fdd76c732065606df6d4f50aa6e9a9638990d0b6
Alena <alena.malodyshnaya@mail.ru>
2022-08-05 22:08:40 +0300

    4 задание

--- 3.js
+++ 3.js
@@ -1,42 +1,44 @@
 'use strict'
 // 1 задание
-let i = 0;
-while(i <= 100 && i >= 0 ){
- console.log(i);
-    i++;
+let i = 0
+while (i <= 100 && i >= 0) {
+  console.log(i)
+  i++
 }
 
 // 2 и 3 задание
 let basket = [
-    {
-product: "skirt",
-size: "S",
-price: 2500
-    },
-    
-    {
-        product: "jeans",
-        size: "S",
-        price: 1500
-    },
-    
-    {
-        product: "T-shit",
-        size: "S",
-        price: 500
-    },
+  {
+    product: 'skirt',
+    size: 'S',
+    price: 2500,
+  },
 
+  {
+    product: 'jeans',
+    size: 'S',
+    price: 1500,
+  },
+
+  {
+    product: 'T-shit',
+    size: 'S',
+    price: 400,
+  },
 ]
 
+function countBasketPrice(basket) {
+  let total_sum = 0
+  basket.forEach(function (item) {
+    total_sum = total_sum + item['price']
+  })
 
-
-function countBasketPrice(arr){
-let a = 0;
-arr.forEach(function(index) {
-  a = a + index["price"];
-});
-    
-return (a) ;
+  return total_sum
 }
 
-console.log(countBasketPrice(basket));
+console.log(countBasketPrice(basket))
+
+// 4 задание
+for (let i = 0; i < 10; console.log(i++)) {
+
+}
