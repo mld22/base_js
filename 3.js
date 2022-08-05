77bce6d5b358b14ae49b7037bac1821072f92861
Alena <alena.malodyshnaya@mail.ru>
2022-08-05 21:33:12 +0300

    2,3 задание

--- 3.js
+++ 3.js
@@ -5,3 +5,38 @@ while(i <= 100 && i >= 0 ){
  console.log(i);
     i++;
 }
+
+// 2 и 3 задание
+let basket = [
+    {
+product: "skirt",
+size: "S",
+price: 2500
+    },
+    
+    {
+        product: "jeans",
+        size: "S",
+        price: 1500
+    },
+    
+    {
+        product: "T-shit",
+        size: "S",
+        price: 500
+    },
+
+]
+
+
+
+function countBasketPrice(arr){
+let a = 0;
+arr.forEach(function(index) {
+  a = a + index["price"];
+});
+    
+return (a) ;
+}
+
+console.log(countBasketPrice(basket));
